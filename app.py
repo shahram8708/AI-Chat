ac152e7df1c7e41c79e7af3726a4b1df998eb09c
shah.ram <ram.coding8@gmail.com>
2024-03-23 14:01:55 +0530

    added commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 import wikipedia
+import requests
 
 app = Flask(__name__)
+
+GOOGLE_API_KEY = 'AIzaSyA5YocXmTntv_LoGm_3ecZbHL-lIz7kmOY'
+SEARCH_ENGINE_ID = 'b45dc313d50ac4cef'
 
 @app.route('/')
 def index():
@@ -11,16 +15,31 @@ def index():
 def get_search_result():
     user_query = request.json.get('user_query') 
     if not user_query:
-        return jsonify({'result': 'Please enter a query.'}), 400
+        return jsonify({'error': 'Please enter a query.'}), 400
 
     try:
-        search_result = wikipedia.summary(user_query)
-        return jsonify({'result': search_result})
-    except wikipedia.exceptions.PageError:
-        return jsonify({'result': 'Sorry, no results found.'}), 404
-    except wikipedia.exceptions.DisambiguationError as e:
-        options = ", ".join(e.options[:3]) 
-        return jsonify({'result': f'Multiple possible results found. Please be more specific. For example: {options}'}), 400
+        wiki_search_result = wikipedia.summary(user_query)
+    except (wikipedia.exceptions.PageError, wikipedia.exceptions.DisambiguationError):
+        wiki_search_result = 'Sorry, If no result is obtained, please consider the result above.'
+
+    try:
+        google_search_result = google_search(user_query)
+    except Exception as e:
+        google_search_result = f'Error fetching results : {str(e)}'
+
+    return jsonify({'wikipedia_result': wiki_search_result, 'google_result': google_search_result})
+
+def google_search(query):
+    url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}"
+    response = requests.get(url)
+    data = response.json()
+    search_results = []
+    if 'items' in data:
+        for item in data['items']:
+            snippet = item.get('snippet', '')
+            search_results.append(snippet)
+    return search_results
+
 
 if __name__ == '__main__':
     app.run(debug=True)
